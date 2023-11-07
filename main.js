aceb50db293eff3dfaad322552fcf44c9c5e8d65
¨Agustin <aguslopez001@hotmail.com>
2023-11-07 20:40:34 -0300

    actualizacion ultima

--- main.js
+++ main.js
@@ -2,49 +2,30 @@ document.addEventListener("DOMContentLoaded", function () {
     let carrito = [];
     let total = 0;
 
-    // Cargar datos de productos utilizando Fetch
-    fetch('productos.json')
-        .then(response => response.json())
-        .then(data => {
-            const productosContainer = document.getElementById("productos-container");
+    const productosContainer = document.querySelectorAll(".producto");
 
-            data.forEach(producto => {
-                const productoElement = document.createElement("div");
-                productoElement.classList.add("product");
-                productoElement.innerHTML = `
-                    <img src="./img/${producto.imagen}" alt="${producto.nombre}">
-                    <h2>${producto.nombre}</h2>
-                    <p>${producto.descripcion}</p>
-                    <p>Precio: $${producto.precio.toFixed(2)}</p>
-                    <button data-nombre="${producto.nombre}" data-precio="${producto.precio}" class="comprar">Añadir al carrito</button>
-                `;
+    productosContainer.forEach(productoElement => {
+        const comprarButton = productoElement.querySelector(".comprar");
 
-                // Agregar evento para añadir productos al carrito
-                const botonComprar = productoElement.querySelector("button.comprar");
-                botonComprar.addEventListener("click", function () {
-                    const nombre = botonComprar.getAttribute("data-nombre");
-                    const precio = parseFloat(botonComprar.getAttribute("data-precio"));
+        comprarButton.addEventListener("click", function () {
+            const nombre = comprarButton.getAttribute("data-nombre");
+            const precio = parseFloat(comprarButton.getAttribute("data-precio"));
 
-                    carrito.push({ nombre, precio });
-                    total += precio;
+            carrito.push({ nombre, precio });
+            total += precio;
 
-                    localStorage.setItem("carrito", JSON.stringify(carrito));
+            localStorage.setItem("carrito", JSON.stringify(carrito));
 
-                    // Actualizar el carrito
-                    actualizarCarrito();
+            actualizarCarrito();
 
-                    Swal.fire({
-                        title: 'Producto Agregado',
-                        text: `"${nombre}" ha sido añadido al carrito.`,
-                        icon: 'success',
-                        confirmButtonText: 'OK'
-                    });
-                });
-
-                productosContainer.appendChild(productoElement);
+            Swal.fire({
+                title: 'Producto Agregado',
+                text: `"${nombre}" ha sido añadido al carrito.`,
+                icon: 'success',
+                confirmButtonText: 'OK'
             });
-        })
-        .catch(error => console.error('Error al cargar los datos: ', error));
+        });
+    });
 
     function actualizarCarrito() {
         const carritoElement = document.getElementById("carrito");
@@ -63,7 +44,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
         totalElement.textContent = `Total: $${total.toFixed(2)}`;
 
-        // Asociar eventos de clic a los botones "Eliminar"
         const botonesEliminar = document.querySelectorAll(".eliminar");
         botonesEliminar.forEach(botonEliminar => {
             botonEliminar.addEventListener("click", function () {
@@ -84,17 +64,42 @@ document.addEventListener("DOMContentLoaded", function () {
     function actualizarCantidad(index, cantidad) {
         const producto = carrito[index];
         total -= producto.precio;
-        producto.precio *= cantidad;
+        producto.precio = parseFloat(cantidad) * producto.precio;
         total += producto.precio;
         localStorage.setItem("carrito", JSON.stringify(carrito));
         actualizarCarrito();
     }
 
-    // Recuperar el carrito desde el almacenamiento local al cargar la página
     const carritoGuardado = JSON.parse(localStorage.getItem("carrito"));
     if (carritoGuardado) {
         carrito = carritoGuardado;
         total = carrito.reduce((acc, item) => acc + item.precio, 0);
         actualizarCarrito();
     }
+    
+    const botonPagar = document.getElementById("botonPagar");
+    botonPagar.addEventListener("click", function () {
+        if (carrito.length > 0) {
+            
+            Swal.fire({
+                title: '¡Compra finalizada!',
+                text: 'Gracias por tu compra.',
+                icon: 'success',
+                confirmButtonText: 'OK'
+            });
+
+            // Reiniciar el carrito
+            carrito = [];
+            total = 0;
+            localStorage.setItem("carrito", JSON.stringify(carrito));
+            actualizarCarrito();
+        } else {
+            Swal.fire({
+                title: 'Carrito vacío',
+                text: 'Agrega productos al carrito antes de pagar.',
+                icon: 'warning',
+                confirmButtonText: 'OK'
+            });
+        }
+    });
 });
